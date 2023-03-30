ccf012f3e9d238092cde533ae86fb19f6612fb90
Yuta Okuma <y.dream.nest@icloud.com>
2023-03-30 12:07:49 +0900

    feat: :sparkles: Install and setting eslint & prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+/** @type {import('prettier')} */
+
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  bracketSameLine: false,
+  trailingComma: "es5",
+};
