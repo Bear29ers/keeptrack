47f78c7f674faba30f5f5b20246be87a5dfa8fc9
Yuta Okuma <y.dream.nest@icloud.com>
2023-04-05 17:03:07 +0900

    feat: :sparkles: Router basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
+import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom';
+
 import './App.css';
+import HomePage from './home/HomePage';
 import ProjectsPage from './projects/ProjectsPage';
 
 const App = () => {
   return (
-    <div className='container'>
-      <ProjectsPage />
-    </div>
+    <Router>
+      <header className='sticky'>
+        <span className='logo'>
+          <img src='/assets/logo-3.svg' alt='logo' width='49' height='99' />
+        </span>
+        <NavLink to='/' className='button rounded'>
+          <span className='icon-home' />
+          Home
+        </NavLink>
+        <NavLink to='/projects' className='button rounded'>
+          Projects
+        </NavLink>
+      </header>
+      <div className='container'>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/projects' element={<ProjectsPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
---
+++ src/home/HomePage.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const HomePage: FC = () => {
+  return <h2>Home</h2>;
+};
+
+export default HomePage;
